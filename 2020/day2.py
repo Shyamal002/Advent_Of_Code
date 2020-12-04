73736fca95dc8a7b7e099b3b14b25f2e6087a7fb
Shyamal002 <shyamal.akruvala@gmail.com>
2020-12-04 14:17:34 +0530

    Added valid answers

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,7 +1,7 @@
 "--- Day 2: Password Philosophy ---"
 from operator import xor
 
-@author = "Shyamal Akruvala"
+__author__ = "Shyamal Akruvala"
 
 def test():
     lines = ['1-3 a: abcde\n', '1-3 b: cdefg\n', '2-9 c: ccccccccc\n']
